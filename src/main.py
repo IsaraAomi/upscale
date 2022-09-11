eed95a6958432078421f78d3b9509d01940ab7b7
IsaraAomi <isara.aomi.87u@gmail.com>
2022-09-11 22:25:31 +0900

    Modified codes.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-from operator import index
 import sys, os, glob
 import pprint
 from PIL import Image
@@ -23,20 +22,19 @@ def get_file_list():
 
 
 def create_graph():
+    """
+    - Args:
+    - Returns:
+    """
     prefix, file_list = get_file_list()
-
-    n_data = len(file_list) # 表示するデータ数
-    # fig, ax = plt.subplots(ncols=2, nrows=3, figsize=(20,20))
     fig = plt.figure(figsize=(30,40), tight_layout=True)
-
-    # fig.suptitle("Comparison of illustration images upscaled by Real-ESRGAN, waifu2x", fontsize=48)
-
+    
     for file_name in file_list:
         i = file_list.index(file_name)
         img = Image.open(prefix + file_name)
         img_crop = img.crop((3300, 1500, 4300, 2500))
         """
-        nrows，ncols，indexは位置と大きさを決めるパラメータで、整数を入れる。台紙Figureを、縦nrows分割・横ncols分割したうちのindex番目の位置に配置されたAxesが返される。グラフを1つしか作らないときは(1, 1, 1)で良い。
+        https://qiita.com/nkay/items/d1eb91e33b9d6469ef51#21-figureadd_subplot
         """
         if i == 0:
             ax = fig.add_subplot(3, 2, 1)
@@ -54,12 +52,6 @@ def create_graph():
         ax.axes.xaxis.set_visible(False)
         ax.axes.yaxis.set_visible(False)
         ax.imshow(img_crop)
-
-    # ax = fig.add_subplot(3, 2, 2)
-    # ax.text(0.2, 0.5, "Comparison of\nillustration images\nupscaled by Real-ESRGAN, waifu2x", fontsize=60, color="black", bbox=(dict(boxstyle="round", fc="white")))
-    # ax.axes.xaxis.set_visible(False)
-    # ax.axes.yaxis.set_visible(False)
-
     
     fig.savefig("../data/comparison.jpg")
 
